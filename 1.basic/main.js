dcc566dbd487763a51ac52d39e147bd310da7003
LimChicken <dlatmdgus1102@gmail.com>
2022-05-26 10:07:03 +0900

    first js

---
+++ 1.basic/main.js
@@ -0,0 +1,11 @@
+    
+    
+    
+    document.getElementById('hello').style.fontSize = '16px';
+  // 문서    id가 hello인 html요소를 가져와라  
+  // html 문서의 id가 hello인 요소를 찾아와 
+  //innerHTML 내부 HTML을 
+  //등호 : 오른쪽을 왼쪽에 넣으라는 뜻 
+
+//HTML 요소 Style 바꾸기  
+  document.getElementById('hi').innerHTML = 'js 고수에요';
---
+++ 2.alert-Box/alert.md
@@ -0,0 +1,1 @@
+![button onclick](<./asstes/ezgif.com-gif-maker%20(1).gif>)
